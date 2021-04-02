956bc2f5b9bb7b84c3390246f505480364812581
mayoujin <mayoujin@gmail.com>
2021-04-02 10:17:38 +0000

    Deploying to gh-pages from  @ 53c4658712f50b42498e9fd46f71b8da2818ce3c 🚀

---
+++ precache-manifest.35f29ec90574f3af9deda1da5198550b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafd24e2c29f7ba7bbdd",
+    "url": "/vue2-js-app-hobby/css/app.9a5f717f.css"
+  },
+  {
+    "revision": "035efcc097c3505c9de4",
+    "url": "/vue2-js-app-hobby/css/chunk-5cebf16e.048bcdfc.css"
+  },
+  {
+    "revision": "2d2b62fa2fc1223d4d6685ea116f37db",
+    "url": "/vue2-js-app-hobby/index.html"
+  },
+  {
+    "revision": "bafd24e2c29f7ba7bbdd",
+    "url": "/vue2-js-app-hobby/js/app.7ab9cde0.js"
+  },
+  {
+    "revision": "035efcc097c3505c9de4",
+    "url": "/vue2-js-app-hobby/js/chunk-5cebf16e.11134e61.js"
+  },
+  {
+    "revision": "734204e577712166e76e",
+    "url": "/vue2-js-app-hobby/js/chunk-vendors.e49fcf1a.js"
+  },
+  {
+    "revision": "675256de768f5146b8b5ab1db5578044",
+    "url": "/vue2-js-app-hobby/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue2-js-app-hobby/robots.txt"
+  }
+]);
